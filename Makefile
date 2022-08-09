f4980a321955f20182734b93533e95381334ffdd
romanofivan <romanofivan@rambler.ru>
2022-08-09 23:01:35 +0300

    add flake8

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ install:
 	poetry install
 brain-games:
 	poetry run brain-games
+lint:
+	poetry run flake8 brain_games
 build:
 	poetry build
 publish:
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,12 +2,15 @@
 
 from brain_games.cli import welcome_user
 
+
 def greeting():
     print("Welcome to the Brain Games!")
+
 
 def main():
     greeting()
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
